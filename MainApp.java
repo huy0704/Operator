1b8d1c3c07bda7b727cd41a79ea693f367d34719
huy <lmtctest01@gmail.com>
2021-11-01 15:44:39 +0700

    first commit

---
+++ MainApp.java
@@ -0,0 +1,29 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a = 100, b = 10;
+ 
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a  * b);
+        System.out.println(a / b);
+                
+        int c = 50;
+        System.out.println(c > a);
+        System.out.println(c > b);
+        System.out.println(c < a);
+        System.out.println(c < b);
+        System.out.println(c <= a);
+        System.out.println(c <= b);
+        System.out.println(c >= a);
+        System.out.println(c >= b);
+        
+        
+	}
+       
+        
+        	
+}
